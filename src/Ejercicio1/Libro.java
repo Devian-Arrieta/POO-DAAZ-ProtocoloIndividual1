bf9d8ecfc9a4f4c395dadfe5332f1f5d559077ba
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 14:22:22 -0500

    17/08/2026 - se añadió el constructor parametrizado a la clase Libro

--- src/Ejercicio1/Libro.java
+++ src/Ejercicio1/Libro.java
@@ -5,10 +5,17 @@ public class Libro {
     String titulo, autor;
     int numeroPaginas;
 
-    // constructor por defecto
+    // 1. constructor por defecto
     public Libro(){
         titulo = "Las Crónicas de Narnia: el león, la bruja y el ropero";
         autor = "Clive Staples Lewis";
         numeroPaginas = 200;
+    }
+
+    // 2. constructor parametrizado
+    public Libro(String titulo, String autor, int numeroPaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
     }
 }
