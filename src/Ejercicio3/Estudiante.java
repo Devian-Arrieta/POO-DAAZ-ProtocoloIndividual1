77b7576bb162af69cebca3606c407297c8c757e6
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 15:49:27 -0500

    17/08/2026 - se creo el tercer constructor donde acepta todos los parametros y se uso this() para llamar a otro constructor

--- src/Ejercicio3/Estudiante.java
+++ src/Ejercicio3/Estudiante.java
@@ -17,4 +17,10 @@ public class Estudiante {
         this.nombre = nombre;
         this.edad = edad;
     }
+
+    // 3. constructor que acepte todos los parámetros y utilice this() para llamar a otro constructor.
+    public Estudiante(String nombre, int edad, String curso){
+        this(nombre, edad);
+        this.curso = curso;
+    }
 }
