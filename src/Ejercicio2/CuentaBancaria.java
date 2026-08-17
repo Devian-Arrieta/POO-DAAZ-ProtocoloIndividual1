0bb6fe343382e963e74f49489ec2d60b6962bc17
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 15:16:52 -0500

    17/08/2026 - se creo un segundo constructor. Este constructor es parametrizado pero solo con 2 parametros, numeroCuenta y tipoCuenta

--- src/Ejercicio2/CuentaBancaria.java
+++ src/Ejercicio2/CuentaBancaria.java
@@ -1,4 +1,19 @@
 package Ejercicio2;
 
 public class CuentaBancaria {
+    String numeroCuenta, tipoCuenta;
+    float saldo;
+
+    // 1. constructor por defecto
+    public CuentaBancaria() {
+        numeroCuenta = "1980-0045-3895";
+        saldo = 100000;
+        tipoCuenta = "cuenta de ahorros";
+    }
+
+    // 2. constructor parametrizado con dos parámetros
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta){
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = numeroCuenta;
+    }
 }
