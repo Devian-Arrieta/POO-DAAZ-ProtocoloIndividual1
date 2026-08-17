0081f323101a96fbbdac4b984ec8aff1e4f33fae
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 15:36:36 -0500

    17/08/2006 - se creó la clase de estudiante y el primer constructor por defecto

---
+++ src/Ejercicio3/Estudiante.java
@@ -0,0 +1,14 @@
+package Ejercicio3;
+
+public class Estudiante {
+
+    String nombre, curso;
+    int edad;
+
+    // 1. constructor por defecto
+    public Estudiante(){
+        nombre = "Devian Arrieta";
+        edad = 19;
+        curso = "2 semestre";
+    }
+}
