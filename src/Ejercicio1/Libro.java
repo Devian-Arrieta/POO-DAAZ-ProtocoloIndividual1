f12eed84ad4bd063e546b38583cbb17d2b193380
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 14:13:58 -0500

    17/08/2026 - Se creó la clase de libro y se creó su le añadió el constructor por defecto

---
+++ src/Ejercicio1/Libro.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Libro {
+
+    String titulo, autor;
+    int numeroPaginas;
+
+    // constructor por defecto
+    public Libro(){
+        titulo = "Las Crónicas de Narnia: el león, la bruja y el ropero";
+        autor = "Clive Staples Lewis";
+        numeroPaginas = 200;
+    }
+}
