d1b7ec451164dfa9b444c02604e4a8b94352d7eb
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-08-17 19:32:25 -0500

    17/08/2026 - Se va a iniciar un nuevo proyecto y ya se instanciaron las clases en el main

---
+++ src/EjercicioFinal/Main.java
@@ -0,0 +1,14 @@
+import Ejercicio1.Libro;
+import Ejercicio2.CuentaBancaria;
+import Ejercicio3.Estudiante;
+
+void main() {
+
+    // Instanciamos e importamos el scanner
+    Scanner teclado = new Scanner(System.in);
+
+    // Instanciamos las clases con los constructores
+    Libro libro = new Libro();
+    CuentaBancaria cuenta = new CuentaBancaria();
+    Estudiante estudiante = new Estudiante();
+}
